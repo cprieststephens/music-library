d568bb962e476297a88c0f8c02e511bfcadfb58a
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-05-15 09:00:32 +0100

    Add test and contoller function for deleting artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -63,4 +63,21 @@ const updateArtist = async (req, res) => {
   }
 };
 
-module.exports = { createArtist, getAllArtists, getArtistById, updateArtist };
+const deleteArtist = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query("DELETE FROM Artists WHERE id = $1 RETURNING *", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getAllArtists, getArtistById, updateArtist, deleteArtist };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,12 +4,13 @@ const {
   getAllArtists,
   getArtistById,
   updateArtist,
+  deleteArtist
 } = require("../controllers/artist");
 
 const artistRouter = express.Router();
 
 artistRouter.route("/").post(createArtist).get(getAllArtists);
 
-artistRouter.route("/:id").get(getArtistById).put(updateArtist);
+artistRouter.route("/:id").get(getArtistById).put(updateArtist).delete(deleteArtist);
 
 module.exports = { artistRouter };
