8879269aba65c1f63bc89fc9d7802cef1721a3f2
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-05-15 06:30:54 +0100

    Add test and controller function for updating artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -2,7 +2,6 @@ const db = require("../db/index");
 
 const createArtist = async (req, res) => {
   const { name, genre } = req.body;
-
   try {
     const {
       rows: [artist],
@@ -42,4 +41,26 @@ const getArtistById = async (req, res) => {
   }
 };
 
-module.exports = { createArtist, getAllArtists, getArtistById };
+const updateArtist = async (req, res) => {
+  const { id } = req.params;
+  const { name, genre } = req.body;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      "UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *",
+      [name, genre, id]
+    );
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getAllArtists, getArtistById, updateArtist };
